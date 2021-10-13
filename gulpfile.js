a6f19e6df4561ea489e742a7d4a6f38562f2feed
vincentasVilk <70482769+vincentasVilk@users.noreply.github.com>
2021-10-13 23:55:02 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('serve', gulp.series(function(done) {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("scss/*.scss", gulp.series('sass'));
+    gulp.watch("*.html").on('change', browserSync.reload);
+
+    done();
+}));
+
+
+gulp.task('sass', gulp.series(function(done) {
+	return gulp.src('scss/style.scss')
+  .pipe(autoprefixer({
+			browsers: ["last 2 versions"],
+      cascade: false
+		}))
+		    .pipe(sass())
+		    .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
+        done();
+}));
+
+gulp.task('default', gulp.parallel('serve', function () {
+    gulp.series('serve')
+}));
